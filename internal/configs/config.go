e2477e6884466a21463c821f5c25bb5e8cdea4cd
yohagos <yosef.hagos@googlemail.com>
2025-11-01 20:59:26 +0100

    updated config an moved to internal

---
+++ internal/configs/config.go
@@ -0,0 +1,24 @@
+package configs
+
+import (
+	"time"
+
+	"github.com/caarlos0/env/v11"
+)
+
+type Config struct {
+	HTTPAddr       string        `env:"HTTP_ADDR" envDefault:":8080"`
+	DatabaseURL    string        `env:"DATABASE_URL" envDefault:"postgres://postgres:postgres@localhost:5432/mpb?sslmode=disable"`
+	AllowedOrigins []string      `env:"ALLOWED_ORIGINS" envDefault:"*"`
+	ReadTimeout    time.Duration `env:"READ_TIMEOUT" envDefault:"15s"`
+	WriteTimeout   time.Duration `env:"WRITE_TIMEOUT" envDefault:"15s"`
+	IdleTimeout    time.Duration `env:"IDLE_TIMEOUT" envDefault:"300s"`
+}
+
+func Load() *Config {
+	cfg := &Config{}
+	if err := env.Parse(cfg); err != nil {
+		panic(err)
+	}
+	return cfg
+}
