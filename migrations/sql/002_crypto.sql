77336c92f3f94d6393d266805b7ed3269f58c51c
yohagos <yosef.hagos@googlemail.com>
2025-11-05 19:38:00 +0100

    update crypto sql script

--- migrations/sql/002_crypto.sql
+++ migrations/sql/002_crypto.sql
@@ -4,8 +4,10 @@ CREATE TABLE IF NOT EXISTS crypto (
     name TEXT NOT NULL,
     current_value DECIMAL(19, 8) NOT NULL,
     previous_value DECIMAL(19, 8),
-    procent DECIMAL(10, 4) NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+    percent DECIMAL(10, 4) NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    is_initial BOOLEAN NOT NULL DEFAULT FALSE,
+    UNIQUE(name, created_at)
 );
 
 CREATE INDEX IF NOT EXISTS idx_crypto_name_initial_created_at ON crypto (name, initial, created_at DESC);
