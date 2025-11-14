3b7b2c993606fc9491074c7d55c373dffed387a9
yohagos <yosef.hagos@googlemail.com>
2025-11-14 19:10:34 +0100

    fix: data type of IsPrimary

--- internal/core/address.go
+++ internal/core/address.go
@@ -9,6 +9,6 @@ type Address struct {
 	City      string    `json:"city" db:"city"`
 	ZipCode   string    `json:"zip_code" db:"zip_code"`
 	Country   string    `json:"country" db:"country"`
-	IsPrimary string    `json:"is_primary" db:"is_primary"`
+	IsPrimary bool      `json:"is_primary" db:"is_primary"`
 	CreatedAt time.Time `json:"created_at" db:"created_at"`
 }
